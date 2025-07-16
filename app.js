5f18e2c9bffdbaaaaaeefcfc8e81260507276e12
KaleeBurchett <kburchett11@ivytech.edu>
2025-07-16 13:03:32 -0400

    Completed M06 Tutorial part 2

--- app.js
+++ app.js
@@ -23,6 +23,17 @@ router.get("/songs", async(req, res) => {
 })
 
 
+//Grab a single sond in the database
+router.get("/songs/:id", async (req, res) => {
+    try {
+        const song = await Song.findById(req.params.id)
+        res.json(song)
+    }
+    catch (err) {
+        res.status(400).send(err)
+    }
+})
+
 router.post("/songs", async(req, res) =>{
     try{
         const song = await new Song(req.body)
@@ -31,6 +42,25 @@ router.post("/songs", async(req, res) =>{
         console.log(song)
     }
     catch(err){ 
+        res.status(400).send(err)
+    }
+})
+
+
+//update is to existing recor/resource/database entry.. it uses a put request
+router.put("/songs/:id", async(req,res) =>{
+    //first we need to find and update the song the frontend wants us to update.
+    //to do this we need to update the id of the song from the request
+    //and find it in the database and update it
+    try {
+        const song = req.body
+        await Song.updateOne({_id: req.params.id},song)
+        console.log(song)
+        res.sendStatus(204)
+
+        
+    }
+    catch(err) {
         res.status(400).send(err)
     }
 })
