1586f7e98a4b8b7ba3ad93e03fae5e44cd6a2878
KaleeBurchett <kburchett11@ivytech.edu>
2025-07-22 10:18:47 -0400

    Added delete function and started on login system

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const Song = require("./models/songs")
 var cors = require('cors')
+//const bodyParser = require('body-parser')
+const jwt = require('jwt-simple')
+const User = require("./models/users")
 
 const app = express();
 app.use(cors())
@@ -9,6 +12,30 @@ app.use(cors())
 app.use(express.json());
 
 const router = express.Router();
+const secret = "supersecret"
+
+//creating a new user
+router.post("/user", async (req, res) => {
+    if(!req.body.username || !req.body.password){
+        res.status(400).json({error: "Missing username or password"})
+    }
+
+    const newUser = await new User({
+        username: req.body.username,
+        password: req.body.password,
+        status: req.body.status
+    })
+
+    try {
+        await newUser.save()
+        console.log(newUser)
+        res.sendStatus(201) //created
+    }
+    
+    catch (err) {
+        res.status(400).send(err)
+    }
+})
 
 //grab all the songs in a database
 router.get("/songs", async(req, res) => {
@@ -61,6 +88,19 @@ router.put("/songs/:id", async(req,res) =>{
         
     }
     catch(err) {
+        res.status(400).send(err)
+    }
+})
+
+router.delete("/songs/:id", async (req, res) => {
+    //method or function in Mongoose/Mongo to delete a single instance of a song or object
+    try{
+        const song = await Song.findById(req.params.id)
+        console.log(song)
+        await Song.deleteOne({ _id: song._id })
+        res.sendStatus(204)
+    }
+    catch (err) {
         res.status(400).send(err)
     }
 })
